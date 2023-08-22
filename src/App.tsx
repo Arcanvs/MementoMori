1714d81dd241eb36f5e02360b16e03ae56f696ed
Jose <jose.osorio.herrera@gmail.com>
2023-08-22 01:09:01 -0400

    orden en semanas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react';
-import { weeksInDates2 } from './utils/weeks';
+import { weeksInDates3 } from './utils/weeks';
 import Week  from './components/Week';
 
 function App() {
-  const [weeks, setWeeks] = useState<boolean[]>([]);
+  const [weeks, setWeeks] = useState<boolean[][]>([]);
   /* for (let i = 0; i<weeksInDates('1960-06-06'); i++){
     persons.push(<Week isPainted={false} />)
   } */
   useEffect(()=>{
-		setWeeks(weeksInDates2('1960-06-06'));
+		setWeeks(weeksInDates3('1960-06-06'));
 	}, []);
 
   return (
     <>
       <h1>Memento Mori</h1>
-      <div>
-        {weeks.map((week, index) => (
-          <Week isPainted={week} key={index} />
-        ))}
+      <div className='dflex'> 
+        {
+          weeks.map((block, index1) => (
+            <div className='wflex' key={index1}>
+              {block.map((week, index2) => (
+                  <Week isPainted={week} key={index2} />
+              ))}
+            </div>
+          )
+        )}
       </div>
     </>
   )
--- src/utils/weeks.ts
+++ src/utils/weeks.ts
@@ -20,3 +20,28 @@ export const weeksInDates2 = (fecha : string) : boolean[] => {
     }
     return weeksArray;
 }
+
+export const weeksInDates3 = (fecha : string) : boolean[][] => {
+    const msInWeek = 1000 * 60 * 60 * 24 * 7;
+    const diffDate = new Date(fecha).getTime() - new Date('2023-08-15').getTime();
+    const weeks = Math.round(Math.abs(diffDate) / msInWeek);
+    
+    let weeksArray = [];
+    const weeksBlock = [];
+    let count = 1;
+    for (let i = 0; i < 4480; i++) {
+        if(count <= 28){
+            if(i <= weeks){
+                weeksArray.push(true);
+            }else{
+                weeksArray.push(false);    
+            }
+            count++;
+        }else{
+            count = 1;
+            weeksBlock.push(weeksArray);
+            weeksArray = [];
+        }
+    }
+    return weeksBlock;
+}
